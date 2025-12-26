ff60ecbdc412892b67c056e3c0f65e49500a3ae1
swasthikg73 <swasthikg32@gmail.com>
2025-12-26 10:53:53 +0530

    Chat model added

---
+++ server/models/User.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  credits: { type: Number, default: 20 },
+});
+
+//Hasing password before saving
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) {
+    return;
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+const UserModel = mongoose.model("User", userSchema);
+
+export default UserModel;
